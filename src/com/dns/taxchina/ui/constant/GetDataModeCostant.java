fb159e43d535ba8a11c2b0377d9e9b912fe96050
lizhen <lizhen@witmob.com>
2014-04-24 15:24:15 +0800

    提交，xlistview

---
+++ src/com/dns/taxchina/ui/constant/GetDataModeCostant.java
@@ -0,0 +1,16 @@
+package com.dns.taxchina.ui.constant;
+/**
+ * 
+ * @author henzil
+ * @version create time:2014-4-24_下午2:37:35
+ * @Description 获取数据时的方式。
+ */
+public interface GetDataModeCostant {
+
+	int LOAD_MODE = 0;
+
+	int REFRESH_MODE = 1;
+
+	int MORE_MODE = 2;
+
+}
