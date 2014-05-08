12a11c618ee2c649edf1f5aa6519821b2d2b8c89
Beckham6273 <1505720001@qq.com>
2014-05-08 17:12:33 +0800

    搜索结果

---
+++ src/com/dns/taxchina/ui/FindResultActivity.java
@@ -0,0 +1,234 @@
+package com.dns.taxchina.ui;
+
+import java.util.HashMap;
+import java.util.List;
+
+import netlib.helper.DataServiceHelper;
+import netlib.model.BaseModel;
+import netlib.model.ErrorModel;
+import netlib.net.DataAsyncTaskPool;
+import netlib.net.DataJsonAsyncTask;
+import netlib.net.DataMode;
+import netlib.util.ErrorCodeUtil;
+import netlib.util.LibIOUtil;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dns.taxchina.R;
+import com.dns.taxchina.service.helper.ModelHelper;
+import com.dns.taxchina.service.model.BaseItemModel;
+import com.dns.taxchina.service.model.FindResultModel;
+import com.dns.taxchina.ui.adapter.FindResultListAdapter;
+import com.dns.taxchina.ui.constant.GetDataModeCostant;
+import com.dns.taxchina.ui.widget.XListView;
+
+/**
+ * @author fubiao
+ * @version create time:2014-5-6_下午1:39:04
+ * @Description 搜索结果 Activity
+ */
+public class FindResultActivity extends BaseActivity implements XListView.IXListViewListener, GetDataModeCostant {
+
+	private XListView listView;
+	private FindResultListAdapter adapter;
+
+	protected DataJsonAsyncTask asyncTask;
+	protected DataAsyncTaskPool dataPool;
+	protected DataServiceHelper dataServiceHelper;
+
+	private int pageNum;
+
+	private String titleStr;
+	private TextView title, back;
+
+	protected ModelHelper jsonHelper;
+
+	public static final int PAGE_SIZE = 20;
+	public static final String LIST_TITLE = "list_title";
+
+	@Override
+	protected void initData() {
+		Intent intent = getIntent();
+		titleStr = intent.getStringExtra(LIST_TITLE);
+		dataPool = new DataAsyncTaskPool();
+		jsonHelper = new ModelHelper(FindResultActivity.this);
+		dataServiceHelper = new DataServiceHelper() {
+
+			@Override
+			public void preExecute() {
+			}
+
+			@Override
+			public void postExecute(String TAG, Object result, Object... params) {
+				Log.v("tag", "updateView");
+				updateView(result, (Integer) params[0]);
+			}
+
+			@Override
+			public Object doInBackground(Object... params) {
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return jsonHelper.parseJson(LibIOUtil.convertStreamToStr(getResources().openRawResource(R.raw.find_result_activity_json)));
+				// return null;
+			}
+		};
+	}
+
+	@Override
+	protected void initViews() {
+		setContentView(R.layout.find_result_activity);
+		title = (TextView) findViewById(R.id.title_text);
+		back = (TextView) findViewById(R.id.back_text);
+		title.setText(titleStr);
+
+		listView = (XListView) findViewById(R.id.x_list_view);
+		adapter = new FindResultListAdapter(FindResultActivity.this, TAG);
+		listView.setAdapter(adapter);
+	}
+
+	@Override
+	protected void initWidgetActions() {
+		back.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+		listView.setXListViewListener(this);
+
+		onLoadEvent();
+	}
+
+	// 程序加载数据
+	private void onLoadEvent() {
+		HashMap<String, String> reqMap = new HashMap<String, String>();
+		reqMap.put("mode", "7");
+		reqMap.put("searchKey", titleStr);
+		reqMap.put("nowPage", 1 + "");
+		reqMap.put("pageSize", PAGE_SIZE + "");
+		jsonHelper.updateParams(getString(R.string.base_url), reqMap, "com.dns.taxchina.service.model.FindResultModel");
+		asyncTask = new DataJsonAsyncTask(TAG, dataServiceHelper, jsonHelper, DataMode.SERVER_LOCAL, true);
+		dataPool.execute(asyncTask, LOAD_MODE);
+	}
+
+	// 下拉刷新加载数据
+	public void onRefresh() {
+		HashMap<String, String> reqMap = new HashMap<String, String>();
+		reqMap.put("mode", "7");
+		reqMap.put("searchKey", titleStr);
+		reqMap.put("nowPage", 1 + "");
+		reqMap.put("pageSize", PAGE_SIZE + "");
+		jsonHelper.updateParams(getString(R.string.base_url), reqMap, "com.dns.taxchina.service.model.FindResultModel");
+		asyncTask = new DataJsonAsyncTask(TAG, dataServiceHelper, jsonHelper, DataMode.SERVER, true);
+		dataPool.execute(asyncTask, REFRESH_MODE);
+	}
+
+	// 点击更多加载数据
+	public void onLoadMore() {
+		HashMap<String, String> reqMap = new HashMap<String, String>();
+		reqMap.put("mode", "7");
+		reqMap.put("searchKey", titleStr);
+		reqMap.put("nowPage", pageNum + 1 + "");
+		reqMap.put("pageSize", PAGE_SIZE + "");
+		jsonHelper.updateParams(getString(R.string.base_url), reqMap, "com.dns.taxchina.service.model.FindResultModel");
+		asyncTask = new DataJsonAsyncTask(TAG, dataServiceHelper, jsonHelper, DataMode.SERVER);// 加载更多不需要缓存
+		dataPool.execute(asyncTask, MORE_MODE);
+	}
+
+	protected void updateView(Object object, int mode) {
+		if (mode == LOAD_MODE || mode == REFRESH_MODE) {
+			listView.stopRefresh();
+		} else if (mode == MORE_MODE) {
+			listView.stopLoadMore();
+		}
+
+		if (object == null) {
+			errorData(mode);
+			return;
+		}
+		if (object instanceof ErrorModel) {// 网络连接失败
+			ErrorModel errorModel = (ErrorModel) object;
+			errorData(mode);
+			// TODO 提示出网络错误
+			Toast.makeText(FindResultActivity.this, ErrorCodeUtil.convertErrorCode(FindResultActivity.this, errorModel.getErrorCode()), Toast.LENGTH_SHORT)
+					.show();
+			return;
+		}
+		BaseModel m = (BaseModel) object;// 服务器返回错误
+		if (m.getResult() > 0) {
+			// TODO 提示出服务器端错误。
+			errorData(mode);
+			Toast.makeText(FindResultActivity.this, m.getMessage(), Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		FindResultModel model = (FindResultModel) object;
+		if (mode == REFRESH_MODE || mode == LOAD_MODE) {
+			onRefreshPostExecute(model.getDataList(), mode, model.isHasNext());
+			pageNum = 1;
+		} else {
+			onMorePostExecute(model.getDataList(), mode, model.isHasNext());
+			pageNum = pageNum + 1;
+		}
+	}
+
+	protected void onRefreshPostExecute(List<BaseItemModel> result, int mode, boolean hasNext) {
+		if (result.isEmpty()) {
+//			listView.onBottomRefreshComplete(PullToRefreshListView.BOTTOM_NO_DATA);
+//			emptyView(mode);
+			adapter.refresh(result);
+		} else {
+			adapter.refresh(result);
+			result.clear();
+			result = null;
+			if (hasNext) {
+//				listView.onBottomRefreshComplete(PullToRefreshListView.BOTTOM_LOAD_MORE);
+			} else {
+//				listView.onBottomRefreshComplete(PullToRefreshListView.BOTTOM_LOAD_FINISH);
+			}
+
+		}
+	}
+
+	protected void onMorePostExecute(List<BaseItemModel> result, int mode, boolean hasNext) {
+		if (result.isEmpty()) {
+//			listView.onBottomRefreshComplete(PullToRefreshListView.BOTTOM_LOAD_FINISH);
+		} else {
+			adapter.addData(result);
+			result.clear();
+			result = null;
+			if (hasNext) {
+//				listView.onBottomRefreshComplete(PullToRefreshListView.BOTTOM_LOAD_MORE);
+			} else {
+//				listView.onBottomRefreshComplete(PullToRefreshListView.BOTTOM_LOAD_FINISH);
+			}
+		}
+	}
+
+	protected void errorData(int mode) {// 数据加载失败
+		if (mode == LOAD_MODE) {
+			// TODO 未完成 网络失败 界面显示，
+//			errorView.show();
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (asyncTask != null) {
+			asyncTask.cancel(true);
+		}
+		if (adapter != null) {
+			adapter.recycleBitmaps();
+		}
+	}
+}
---
+++ src/com/dns/taxchina/ui/adapter/FindResultListAdapter.java
@@ -0,0 +1,142 @@
+package com.dns.taxchina.ui.adapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import netlib.net.AsyncTaskLoaderImage;
+import netlib.net.AsyncTaskLoaderImage.BitmapImageCallback;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dns.taxchina.R;
+import com.dns.taxchina.service.model.BaseItemModel;
+
+/**
+ * @author fubiao
+ * @version create time:2013-10-16_上午11:35:00
+ * @Description 搜索结果 adapter
+ */
+public class FindResultListAdapter extends BaseAdapter {
+
+	private Context context;
+	private String TAG;
+	private Handler mHandler = new Handler();
+	private Map<String, String> urlMap;
+	private List<BaseItemModel> list = new ArrayList<BaseItemModel>();
+
+	public FindResultListAdapter(Context context, String TAG) {
+		this.context = context;
+		this.TAG = TAG;
+		urlMap = new HashMap<String, String>();
+	}
+
+	public void refresh(List<BaseItemModel> arg0) {
+		list.clear();
+		list.addAll(arg0);
+		notifyDataSetChanged();
+	}
+
+	public void addData(List<BaseItemModel> arg0) {
+		list.addAll(arg0);
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+	@Override
+	public BaseItemModel getItem(int position) {
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder = null;
+		if (convertView == null) {
+			convertView = LayoutInflater.from(context).inflate(R.layout.find_result_list_item, null);
+			holder = new ViewHolder(convertView);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.update(list.get(position), position);
+		return convertView;
+	}
+
+	public class ViewHolder {
+		private ImageView icon;
+		private TextView title, text;
+		private View line;
+		public BaseItemModel model;
+
+		public ViewHolder(View view) {
+			icon = (ImageView) view.findViewById(R.id.find_result_item_img);
+			title = (TextView) view.findViewById(R.id.find_result_item_title_text);
+			text = (TextView) view.findViewById(R.id.find_result_item_info_text);
+			line = view.findViewById(R.id.find_result_item_line);
+		}
+
+		public void update(final BaseItemModel baseItemModel, int positon) {
+			model = baseItemModel;
+			title.setText(model.getTitle());
+			text.setText(model.getInfo());
+
+			icon.setImageResource(R.drawable.default_103x66);
+			if (model.getImage() != null && !model.getImage().equals("")) {
+				AsyncTaskLoaderImage.getInstance(context).loadAsync(TAG, model.getImage(), icon, new BitmapImageCallback() {
+					@Override
+					public void onImageLoaded(final Bitmap bitmap, final String url) {
+						mHandler.post(new Runnable() {
+							@Override
+							public void run() {
+								if (bitmap != null && !bitmap.isRecycled()) {
+									if (context != null) {
+										urlMap.put(model.getImage(), "");
+										TransitionDrawable td = new TransitionDrawable(new Drawable[] { new ColorDrawable(android.R.color.transparent),
+												new BitmapDrawable(context.getResources(), bitmap) });
+										icon.setImageDrawable(td);
+										td.startTransition(200);
+									} else {
+										bitmap.recycle();
+									}
+								} else {
+									icon.setImageResource(R.drawable.default_103x66);
+								}
+							}
+						});
+					}
+				});
+			}
+
+			if (positon == getCount() - 1) {
+				line.setVisibility(View.INVISIBLE);
+			} else {
+				line.setVisibility(View.VISIBLE);
+			}
+		}
+	}
+
+	public void recycleBitmaps() {
+		AsyncTaskLoaderImage.getInstance(context).recycleBitmaps(TAG, urlMap);
+	}
+}
