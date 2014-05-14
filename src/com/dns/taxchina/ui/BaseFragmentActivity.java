49dfd74d3792829612576321e452162858b64084
Beckham <fubiao@dns.com.cn>
2014-05-14 21:51:07 +0800

    哭脸  + 禁止上拉加载

--- src/com/dns/taxchina/ui/BaseFragmentActivity.java
+++ src/com/dns/taxchina/ui/BaseFragmentActivity.java
@@ -1,20 +1,28 @@
 package com.dns.taxchina.ui;
 
-import com.dns.taxchina.R;
-import com.dns.taxchina.ui.widget.LoadingDialog;
-
 import netlib.util.ResourceUtil;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.dns.taxchina.R;
+import com.dns.taxchina.ui.widget.LoadingDialog;
 
 public abstract class BaseFragmentActivity extends FragmentActivity {
 
+	protected LoadingDialog loadingDialog;
 	protected ResourceUtil resourceUtil;
 	protected String TAG;
-	protected LoadingDialog loadingDialog;
+	protected RelativeLayout failBox;
+	protected ImageView failImg;
+	protected TextView failText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,44 @@ public abstract class BaseFragmentActivity extends FragmentActivity {
 
 	protected void initBaseViews() {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+	}
+	
+	protected void initFailView(View view) {
+		failBox = (RelativeLayout) view.findViewById(R.id.no_data_box);
+		failImg = (ImageView) view.findViewById(R.id.no_data_img);
+		failText = (TextView) view.findViewById(R.id.no_data_text);
+	}
+	
+	protected void emptyFailView() {
+		emptyFailView(getString(R.string.no_content));
+	}
+	
+	protected void emptyFailView(String emptyStr) {
+		failBox.setVisibility(View.VISIBLE);
+		failImg.setBackgroundResource(R.drawable.no_data_img);
+		failText.setText(emptyStr);
+	}
+	
+	public interface OnEmptyFailViewClickedListener{
+		void onFailViewClicked();
+	}
+
+	protected void errorFailView(final OnEmptyFailViewClickedListener listener) {
+		failBox.setVisibility(View.VISIBLE);
+		failImg.setBackgroundResource(R.drawable.no_data_img);
+		failText.setText(getString(R.string.cache_load_more));
+		failBox.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if(listener != null)
+					listener.onFailViewClicked();
+			}
+		});
+	}
+
+	protected void resetFailView() {
+		failBox.setVisibility(View.GONE);
 	}
 
 	protected abstract void initData();
